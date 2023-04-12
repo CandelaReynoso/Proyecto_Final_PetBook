71d29e326149fb374edc52e2f738d626ad243f29
Adresspy <adressx23@gmail.com>
2023-04-12 14:05:46 -0500

    changes

--- client/src/COMPONENTS/CARD/Card.jsx
+++ client/src/COMPONENTS/CARD/Card.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function Card() {
-  return <div>yujuuuuuuuuuu</div>;
-}
+const Card = ({ name }) => {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://dog.ceo/api/breeds/image/random%27")
+      .then(response => {
+        setImage(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="card">
+      <img src={image} alt="dog" />
+      <p>{name}</p>
+    </div>
+  );
+};
+
+export default Card;
