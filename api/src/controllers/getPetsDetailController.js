fe8a95f02d98f52e65b9ab1d80d3628177463f55
lucas salvatierra <lucassnahuel1759@gmail.com>
2023-04-13 18:53:36 -0300

    PB-29 get pet por id

--- api/src/controllers/getPetsDetailController.js
+++ api/src/controllers/getPetsDetailController.js
@@ -1,5 +1,8 @@
-const getPetsDetailController = async  (id) =>{
+const { Pet } = require("../DataBase/db.js");
 
-}
+const getPetsDetailController = async (id) => {
+  let response = await Pet.findByPk(id);
+  return response;
+};
 
-module.exports = getPetsDetailController
+module.exports = getPetsDetailController;
