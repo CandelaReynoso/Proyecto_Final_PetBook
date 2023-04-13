419cc6cc5435c5b9d34d2c8dfcc14b830789d276
CandelaReynoso <mariacandelamusica@gmail.com>
2023-04-12 21:55:56 -0300

    cartas renderizadas conbase de datos creada en array

--- client/src/COMPONENTS/CARD/Card.jsx
+++ client/src/COMPONENTS/CARD/Card.jsx
@@ -1,24 +1,18 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const Card = ({ name }) => {
-  const [image, setImage] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("https://dog.ceo/api/breeds/image/random%27")
-      .then(response => {
-        setImage(response.data.message);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
+const Card = ({ mascota }) => {
+  const { image, name, breed, height, weight, lifespan, species, adopted } = mascota;
 
   return (
     <div className="card">
-      <img src={image} alt="dog" />
+      <img src={image} alt="mascota" />
       <p>{name}</p>
+      <p>{breed}</p>
+      <p>{height}</p>
+      <p>{weight}</p>
+      <p>{lifespan}</p>
+      <p>{species}</p>
+      <p>{adopted ? "Adoptado" : "Disponible"}</p>
     </div>
   );
 };
