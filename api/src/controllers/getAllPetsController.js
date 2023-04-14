63d544b1f09f3d5104789fd01d777fc6f4dd0438
lucas salvatierra <lucassnahuel1759@gmail.com>
2023-04-13 22:35:55 -0300

    correccion linea 7 getAllPetsController

--- api/src/controllers/getAllPetsController.js
+++ api/src/controllers/getAllPetsController.js
@@ -1,16 +1,14 @@
-const {Pet} = require("../DataBase/db.js")
+const { Pet } = require("../DataBase/db.js");
 
 const getAllPetsController = async () => {
-    let dbPets = [];
+  let dbPets = [];
 
-    dbPets = await Pet.findAll({
-        
-    })
+  dbPets = await Pet.findAll();
 
-    if(dbPets.length === 0){
-        throw new Error('Error getting the pets from DataBase.');
-    } else {
-        return dbPets;
-    }
+  if (dbPets.length === 0) {
+    throw new Error("Error getting the pets from DataBase.");
+  } else {
+    return dbPets;
+  }
 };
 module.exports = getAllPetsController;
