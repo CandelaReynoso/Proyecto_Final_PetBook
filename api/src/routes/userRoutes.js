69aca326be92835a4ff388434cae22f7368d3d9c
ruldiaz <rulhdiaz@gmail.com>
2023-04-14 23:33:01 -0600

    user handler updated, validator updated, user routes completed

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { userHandlerGet, userHandlerPost, userHandlerPut, userHandlerDelete } = require('../handlers/userHandler');
 const { check } = require('express-validator');
 const { validateAttributes } = require('../middlewares/validateAttributes');
-const { isRoleValid, isEmailValid } = require('../helpers/dbValidators');
+const { isRoleValid, isEmailValid, userByIdExists } = require('../helpers/dbValidators');
 
 
 const userRoutes = Router();
@@ -19,8 +19,17 @@ userRoutes.post('/', [
     validateAttributes  // middleware that validates attributes before passing to the handler
 ],userHandlerPost );
 
-userRoutes.put('/:id', userHandlerPut );
+userRoutes.put('/:id', [
+    check('id', 'Not a valid ID').isUUID(),
+    check('id').custom( userByIdExists ),
+    check('role').custom(isRoleValid), // validate role
+    validateAttributes
+], userHandlerPut );
 
-userRoutes.delete('/', userHandlerDelete );
+userRoutes.delete('/:id', [
+    check('id', 'Not a valid ID').isUUID(),
+    check('id').custom( userByIdExists ),
+    validateAttributes
+], userHandlerDelete );
 
 module.exports = userRoutes;
