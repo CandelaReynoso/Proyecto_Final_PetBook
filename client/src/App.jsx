6a4905c49097a0ced90639788bd2f56b1c01ff34
CandelaReynoso <mariacandelamusica@gmail.com>
2023-04-19 10:27:46 -0300

    creacion Form de contacto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './COMPONENTS/LOGIN/Login';
 import Registration from './COMPONENTS/REGISTRATION/Registration';
 import About from './COMPONENTS/ABOUT/About'
 import FormAdoption from './COMPONENTS/FORMS/FormAdoption';
-
+import FormContact from './COMPONENTS/FORMS/FormContact'
 import './index.css'
 
 
@@ -29,6 +29,7 @@ function App() {
         <Route path="/register" element={<Registration />} />
         <Route path='/about' element={<About />} />
         <Route path="/FormAdoption" element={<FormAdoption />} />
+        <Route path="/FormContact" element={<FormContact/>} />
 
       </Routes>
     </div>
