7d9637d540aa405128ac7eee6223a87b673e043c
trechiStrix <matydavila_77@hotmail.com>
2023-04-20 12:24:32 -0300

    npm rebuild

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { sequelize } = require("./src/DataBase/db.js");
 const { Role } = require('./src/DataBase/db.js');
 require("dotenv").config();
-const { PORT } = process.env || 3001; //Puerto 3001 por defecto.
+const { PORT } = process.env || 3001; //Puerto 3001 en caso de no estar el link a Railway.
 
 //Conectado a Railway.
 sequelize.sync({alter:true }).then( async () => {
