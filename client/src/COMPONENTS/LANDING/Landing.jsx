d71812be00eab0eb1126bb27d0a030fadfbe3ff0
nikkiburgos <burgosveganicole@gmail.com>
2023-04-20 14:37:17 -0300

    cambios en landing

--- client/src/COMPONENTS/LANDING/Landing.jsx
+++ client/src/COMPONENTS/LANDING/Landing.jsx
@@ -41,7 +41,10 @@ export default function Landing() {
       {showLogin && (
         <div className="opacity-[99%] flex justify-center items-center h-screen ">
           <div className="mt-[-75rem]">
-          <button onClick={handleCloseClick}>X</button>
+          <button className="btn btn-square bg-white" onClick={handleCloseClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+          </button>
+          {/* <button className="" onClick={handleCloseClick}>X</button> */}
             <Login />
           </div>
         </div>
