9ac448d8412360cc8d17aff3c7b30919d77dc414
RaulDiaz <rulhdiaz@gmail.com>
2023-04-20 09:21:42 -0600

    contact form updated, anyone can send emails, not just logged users

--- api/src/DataBase/Models/Email.js
+++ api/src/DataBase/Models/Email.js
@@ -33,7 +33,7 @@ module.exports = (sequelize) => {
       },
       user: {
         type: DataTypes.UUID,
-        allowNull: false,
+        allowNull: true,
       }
     },
     { timestamps: false }
