6895ea5cff6c4ba94a49d18598f47dce59de328c
CandelaReynoso <mariacandelamusica@gmail.com>
2023-04-23 14:38:57 -0300

    modificando estilos detail

--- client/src/COMPONENTS/DETAIL/Detail.jsx
+++ client/src/COMPONENTS/DETAIL/Detail.jsx
@@ -4,6 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchPetDetailSuccess } from '../../Redux/actions';
 import { Link } from 'react-router-dom';
 import styles from '../DETAIL/Detail.module.css';
+import Header from '../HEADER/Header';
+import HeaderLogin from "../HEADER/HeaderLogin";
+import Footer from "../FOOTER/Footer";
 
 
 function Detail() {
@@ -18,45 +21,56 @@ function Detail() {
   }, [id]);
 
   return (
-    <div className={styles.background}>
+    
+    <div  className="bg-[url('/backdonations1.png')] bg-no-repeat w-[100hv] h-[100hv]">
+        <div>
+          {localStorage.getItem('token') ? <HeaderLogin className='mb-4' /> : <Header className="mb-4" /> }    
+        </div>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div>
-          <h3 className={styles.title}>{pet.name}</h3>
-          <div >
-            <img className={styles.img} src={pet.image} alt={pet.name} />
+        
+        <div className="card w-72 bg-base-100 shadow-xl m-4">
+          <h3 className="card-title text-black font-['candara']">{pet.name}</h3>
+          <div>
+            <img className="card-body" src={pet.image} alt={pet.name} />
           </div>
           
           <Link to='/home'>
           <img className = {styles.imgFoot}  src="../../../public/patita3.png" alt="patita de perro" />
           </Link>
-
-          <ul className={styles.list}> 
-          <div className={styles.list2}>
-          <li>Specie: {pet.specie}</li>
-          <li>Gender: {pet.gender}</li>
-          <li>Size: {pet.size}</li>
-          <li>Weight: {pet.weight} kg</li>
-          <li>Age: {pet.age} years</li>
-          <li>Adopted: {pet.adopted ? 'Yes' : 'No'}</li>
-          </div>
-         
-        </ul>
         </div>
       )}
       <br>
       </br>
+      <div className="flex justify-center">
+  <ul className="text bg-primary w-fit rounded-full max-w-lg mx-auto">
+    <div className="text-center">
+      <li>Specie: {pet.specie}</li>
+      <li>Gender: {pet.gender}</li>
+      <li>Size: {pet.size}</li>
+      <li>Weight: {pet.weight} kg</li>
+      <li>Age: {pet.age} years</li>
+      <li>Adopted: {pet.adopted ? 'Yes' : 'No'}</li>
+    </div>
+  </ul>
+</div>
+
         <Link to = "/FormAdoption">
-         <button className={styles.buttonAdoptMe}>ADOPT A PET!</button>  
+        <div className="flex justify-end w-full">
+          <button className="btn btn-primary sm:w-auto">ADOPT A PET!</button>
+        </div>
         </Link>
         <br>
         </br>
+        <div className="flex justify-end w-full">
        <Link to ="/donate">
-        <button className={styles.buttonSponsor}>DONATE!</button>
+        <button className="btn btn-primary">DONATE!</button>
        </Link>
-        
+       </div>
+       <div><Footer/></div>
     </div>
+    
   );
 }
 
