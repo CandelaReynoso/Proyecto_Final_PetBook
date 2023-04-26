5641ef65a13934a2bada209ad752d7b4c8f305ef
RaulDiaz <rulhdiaz@gmail.com>
2023-04-26 07:32:06 -0600

    db

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -5,27 +5,27 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
 
-  const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-    {
-      logging: false,
-      native: false,
-    }
- );
+//   const sequelize = new Sequelize(
+//     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//     {
+//       logging: false,
+//       native: false,
+//     }
+//  );
 
-// const sequelize = new Sequelize(
-//  DB_DEPLOY,
-//  {
-//   logging: false,
-//    native: false,
-//      dialectOptions: {
-//        ssl:{
-//          require: true,
-//          rejectUnauthorized: false // Add this line
-//        }
-//      }
-//  }
-// );
+const sequelize = new Sequelize(
+ DB_DEPLOY,
+ {
+  logging: false,
+   native: false,
+     dialectOptions: {
+       ssl:{
+         require: true,
+         rejectUnauthorized: false // Add this line
+       }
+     }
+ }
+);
 
 const basename = path.basename(__filename);
 
