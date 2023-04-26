ba552472218497695060e8f8186d50edb52497ad
CandelaReynoso <mariacandelamusica@gmail.com>
2023-04-26 11:53:16 -0300

    estilos form de adopcion

--- client/src/COMPONENTS/FORMS/FormAdoption.jsx
+++ client/src/COMPONENTS/FORMS/FormAdoption.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useParams } from 'react-router-dom';
+import Header from "../HEADER/Header";
+import HeaderLogin from "../HEADER/HeaderLogin";
+import Footer from "../FOOTER/Footer";
+
 
 const adoptionSchema = Yup.object().shape({
   name: Yup.string().required('Name is required'),
@@ -58,9 +62,13 @@ const AdoptionForm = (props) => {
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <p>Hello <strong>{nickname}</strong>, thank you for your interest. Please enter your data.</p>
-      <div>
+    <div>
+    <div> {localStorage.getItem('token') ? <HeaderLogin className='mb-4' /> : <Header className="mb-4" /> }</div>
+    <div> 
+    <p className="subtitle" >Hello <strong>{nickname}</strong>, thank you for your interest. Please enter your data.</p>
+   </div>
+    <form  className='max-w-[400px] w-full mx-auto rounded-lg bg-white  p-4' onSubmit={formik.handleSubmit}>
+      <div className='divForminput'>
         <label htmlFor="name">Name:</label>
         <input
           id="name"
@@ -74,7 +82,7 @@ const AdoptionForm = (props) => {
         ) : null}
       </div>
 
-      <div>
+      <div className='divForminput'>
         <label htmlFor="email">Email:</label>
         <input
           id="email"
@@ -89,7 +97,7 @@ const AdoptionForm = (props) => {
         ) : null}
       </div>
 
-      <div>
+      <div className='divForminput'>
         <label htmlFor="address">Address:</label>
         <input
           id="address"
@@ -103,7 +111,7 @@ const AdoptionForm = (props) => {
         ) : null}
       </div>
 
-      <div>
+      <div className='divForminput'>
         <label htmlFor="postalCode">Postal Code:</label>
         <input
           id="postalCode"
@@ -117,7 +125,7 @@ const AdoptionForm = (props) => {
         ) : null}
       </div>
 
-      <div>
+      <div className='divForminput'>
         <label htmlFor="age">Age:</label>
         <input
           id="age"
@@ -128,7 +136,7 @@ const AdoptionForm = (props) => {
         />
       </div>
 
-<div>
+<div className='divForminput'>
   <label htmlFor="facebook">Facebook:</label>
   <input
     id="facebook"
@@ -142,7 +150,7 @@ const AdoptionForm = (props) => {
   ) : null}
 </div>
 
-<div>
+<div className='divForminput'>
   <label htmlFor="instagram">Instagram:</label>
   <input
     id="instagram"
@@ -156,7 +164,7 @@ const AdoptionForm = (props) => {
   ) : null}
 </div>
 
-<div>
+<div className='divForminput'>
   <label htmlFor="acceptAgreement">
     I accept the terms and conditions:
     <input
@@ -171,12 +179,22 @@ const AdoptionForm = (props) => {
     <div>{formik.errors.acceptAgreement}</div>
   ) : null}
 </div>
-
+<div className="buttonSubtmit text-center ">
 <button type="submit" disabled={formik.isSubmitting}>
   {formik.isSubmitting ? 'Submitting...' : 'Submit'}
 </button>
+</div>
 </form>
-);
+   {/* IMAGEN */}
+   <div className='hidden sm:block'>  
+              <img className='w-[100%] h-full  object-cover' src="/gatitoform.png" alt="gatito que lee" />
+            </div>
+<div>
+  <Footer></Footer>
+</div>
+
+      </div>
+  )
 };
 
 export default AdoptionForm;
