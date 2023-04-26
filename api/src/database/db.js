fd9ffd2a9325796fdf693a30ae02d77ae85d7a35
RaulDiaz <rulhdiaz@gmail.com>
2023-04-26 12:43:29 -0600

    db

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
-/* 
+
 
 //   const sequelize = new Sequelize(
 //     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
@@ -13,19 +13,19 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 //     }
 //  );
 
-const sequelize = new Sequelize(
- DB_DEPLOY,
- {
-  logging: false,
-   native: false,
-     dialectOptions: {
-       ssl:{
-         require: true,
-         rejectUnauthorized: false // Add this line
-       }
-     }
-
-  ); */
+// const sequelize = new Sequelize(
+//  DB_DEPLOY,
+//  {
+//   logging: false,
+//    native: false,
+//      dialectOptions: {
+//        ssl:{
+//          require: true,
+//          rejectUnauthorized: false // Add this line
+//        }
+//      }
+//     }
+//   ); 
 
 const sequelize = new Sequelize(
  DB_DEPLOY,
