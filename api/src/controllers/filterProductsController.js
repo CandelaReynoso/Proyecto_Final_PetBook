c611745b71c9f804c096b53aa16d21b950212c8e
trechi <matydavila_77@hotmail.com>
2023-04-26 17:48:45 -0300

    filtros de productos andando

--- api/src/controllers/filterProductsController.js
+++ api/src/controllers/filterProductsController.js
@@ -37,21 +37,28 @@ async function filterProductsController({
               return response;
         }
 
+        if (!price || !sortCriterion) {
+            let toDelete = [];
+            where = {
+                name: name ? name : toDelete.push("name"),
+                specie: specie ? specie : toDelete.push("specie"),
+                available: available ? available : toDelete.push("available"),
+                category: category ? category :toDelete.push("category"),
+            };
+            
+            for (let i = 0; i < toDelete.length; i++) {
+              delete where[toDelete[i]];
+            }
+           
+            response = await Product.findAll({
+              where
+            });
+
+            return response;
+          }
         
     }
 
-    
-    // const sort = price;
-    // const criterion = sortCriterion;
-
-    // const results = await Product.findAll({
-    //     where: filter,
-    //     order: [[criterion, sort]]
-    //   });
-
-    //   if(results) return results;
-
-    //   throw new Error('Error filtering your products');
     return;
 }
 
