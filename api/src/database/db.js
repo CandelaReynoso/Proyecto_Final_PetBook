b15602d0c158dce730cd7c51885fc1f3eff0f1dc
trechi <matydavila_77@hotmail.com>
2023-04-26 15:36:02 -0300

    conectados a localhost

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -14,27 +14,27 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 //  );
 
 
-const sequelize = new Sequelize(
- DB_DEPLOY,
- {
-  logging: false,
-   native: false,
-     dialectOptions: {
-       ssl:{
-         require: true,
-       }
-     }
-    }
-  ); 
-
 // const sequelize = new Sequelize(
 //  DB_DEPLOY,
 //  {
 //   logging: false,
 //    native: false,
+//      dialectOptions: {
+//        ssl:{
+//          require: true,
+//        }
+//      }
+//     }
+//   ); 
 
-//  }
-// );
+const sequelize = new Sequelize(
+ DB_DEPLOY,
+ {
+  logging: false,
+   native: false,
+
+ }
+);
 
 const basename = path.basename(__filename);
 
