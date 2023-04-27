97170d5b3dbec02026794e573dd1eff1dad3b244
CamilinLopez <camilollc98@gmail.com>
2023-04-27 08:08:43 -0500

    subir prductos

--- api/src/controllers/uploadProductsCotroller.js
+++ api/src/controllers/uploadProductsCotroller.js
@@ -1,34 +1,15 @@
 const { Product } = require("../database/db");
-const path = require('path');
-const fs = require('fs');
+const productos = require("../../productosJSON.json");
 
 
-const { promisify } = require('util');
-const readFileAsync = promisify(fs.readFile);
-
-
-const readProductos = async () => {
-  const pathProductos = path.join('D:\\', 'henry', 'PF', 'Proyecto_Final_PetBook', 'api', 'productosJSON.json');
-  try {
-    const productosJSON = await readFileAsync(pathProductos, "utf-8");
-    const productos = JSON.parse(productosJSON);
-    // console.log(productos[5]);
-    return productos;
-
-  } catch (error) {
-    return { error: error };
-  }
-}
 
 const uploadProductos = async () => {
   try {
-    const productos = await readProductos();
-    if (productos.error) throw new Error(productos.error);
+    if(!productos.length) throw new Error("No hay productos")
+    
+    productos.forEach(item => item.userId = "0b3a68cd-b171-4451-aad4-dbf744fa3a19")
 
     const count = await Product.count();
-
-    productos.forEach( item => item.userId = "0b3a68cd-b171-4451-aad4-dbf744fa3a19")
-
     if (count === 0) await Product.bulkCreate(productos);
 
   } catch (error) {
@@ -42,4 +23,4 @@ const lengthProducts = async () => {
 }
 
 
-module.exports = { readProductos, uploadProductos, lengthProducts };
+module.exports = { uploadProductos, lengthProducts };
