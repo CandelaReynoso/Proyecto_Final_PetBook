1706eb23891c5ecc0466e6d46e602e2d920846b9
CandelaReynoso <mariacandelamusica@gmail.com>
2023-04-27 12:52:10 -0300

    pagina thanks lista

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,6 @@ import FormHistory from "./COMPONENTS/HISTORY-ADOPTADOS/FormHistory";
 
 import Admin from "./COMPONENTS/ADMIN/Admin";
 import ErrorPage from "./COMPONENTS/UTILS/ErrorPage";
-import Thanks from "./COMPONENTS/THANKS/Thanks";
 
 //Instancia de axios para Railway.
 // axios.defaults.baseURL = "https://proyectofinalpetbook-production.up.railway.app";
@@ -66,7 +65,6 @@ function App() {
         <Route path="/chat" element={<Chatbot />} />
         <Route path="/admin" element={<Admin/>} />
         <Route path='/error' element={<ErrorPage />} />
-        <Route path='/error' element={<Thanks />} />
       </Routes>
     </div>
   );
