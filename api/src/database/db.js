3fb9b1ab448f2c99dd1b102ba8fc6dc25f3cd986
trechiStrix <matydavila_77@hotmail.com>
2023-04-28 19:05:41 -0300

    .

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -5,26 +5,26 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
 
-   const sequelize = new Sequelize(
-     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-     {
-       logging: false,
-       native: false,
-     }
-  );
+  //  const sequelize = new Sequelize(
+  //    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  //    {
+  //      logging: false,
+  //      native: false,
+  //    }
+  // );
 
-//const sequelize = new Sequelize(
- //DB_DEPLOY,
- //{
- // logging: false,
- //  native: false,
-    // dialectOptions: {
-    //   ssl:{
-    //     require: true,
-    //   }
-    // }
- //}
-//);
+const sequelize = new Sequelize(
+ DB_DEPLOY,
+ {
+ logging: false,
+  native: false,
+    dialectOptions: {
+      ssl:{
+        require: true,
+      }
+    }
+ }
+);
 
 const basename = path.basename(__filename);
 
