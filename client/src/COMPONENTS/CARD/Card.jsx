45bd67adc9403dd8abc8636b65fa10186756a6ea
lucas salvatierra <lucassnahuel1759@gmail.com>
2023-05-03 02:01:47 -0300

    implemente gif cuando el apartado de favoritos esta vacio para q se muestre, boton reemplazado a pedido de niki para favoritos ya que no se puede setear el estado de los corazones por cuestiones de logica del paginado llevaria bastante tiempo ver el como se podria implementar entonces queda asi

--- client/src/COMPONENTS/CARD/Card.jsx
+++ client/src/COMPONENTS/CARD/Card.jsx
@@ -68,26 +68,26 @@ const Card = (pet) => {
           />
           <div>
             {localStorage.getItem("token") && (
-              <button
-                onClick={handleFavorite}
-                className="btn btn-xs btn-accent"
-              >
-                Favorites
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
-              </button>
+             <button
+             onClick={handleFavorite}
+             class="inline-block px-4 py-2 btn-accent"
+           >
+             Favorites
+             <svg
+               xmlns="http://www.w3.org/2000/svg"
+               className="h-6 w-6"
+               fill="none"
+               viewBox="0 0 24 24"
+               stroke="currentColor"
+             >
+               <path
+                 strokeLinecap="round"
+                 strokeLinejoin="round"
+                 strokeWidth="2"
+                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+               />
+             </svg>
+           </button>
             )}
           </div>
         </figure>
